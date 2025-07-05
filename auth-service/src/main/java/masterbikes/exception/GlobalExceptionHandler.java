c8d40d16f88edc11d6248f3c0376fe3ae4dadb99
HecAguilaV <he.aguila@duocuc.cl>
2025-07-04 23:24:19 -0400

    test

--- auth-service/src/main/java/masterbikes/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/masterbikes/exception/GlobalExceptionHandler.java
@@ -2,18 +2,15 @@ package masterbikes.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException ex) {
@@ -28,8 +25,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         body.put("mensaje", "Acceso denegado: no tienes permisos suficientes.");
         return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);
     }
-
-    // Eliminado para evitar ambigüedad con ResponseEntityExceptionHandler
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneralException(Exception ex) {
